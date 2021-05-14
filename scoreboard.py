e4508b8716c6e3ec7a7b702d4ce7154c67d791e4
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2021-05-13 19:09:26 -0500

    storage for the high score

--- scoreboard.py
+++ scoreboard.py
@@ -7,8 +7,10 @@ class Scoreboard( Turtle ):
 
     def __init__( self ):
         super().__init__()
+
         self.score = 0
-        self.high_score = 0
+        self.get_highscore()
+        
         self.color( "white" )
         self.hideturtle()
         self.penup()
@@ -26,5 +28,15 @@ class Scoreboard( Turtle ):
     def reset( self ):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.set_highscore()
         self.score = 0
         self.update()
+
+    def get_highscore( self ):
+        with open( "data.txt" ) as file:
+            self.high_score = int( file.read() )
+
+    def set_highscore( self ):
+        with open( "data.txt", mode="w" ) as file:
+            file.write( str( self.high_score ) )
+
